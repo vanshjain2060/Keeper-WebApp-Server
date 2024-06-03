064e0def1118ab4eec2f871a2a64dc6e5ecc16d9
vanshjain2060 <vanshjain0311@gmail.com>
2024-06-03 13:48:26 +0530

    cors updated

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ require("dotenv").config();
 const app = express();
 
 const cors = require("cors");
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000'
+}));
 app.use(express.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
