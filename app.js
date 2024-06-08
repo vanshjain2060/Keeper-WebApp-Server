4c461b54124893f28d053652ae984242079a64c5
vanshjain2060 <vanshjain0311@gmail.com>
2024-06-09 01:02:37 +0530

    refresh bug fixed

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 import { User } from "./model/user.model.js";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
-import md5 from "md5";
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import { auth } from "./middleware/auth.middleware.js" 
+import { auth } from "./middleware/auth.middleware.js";
 
 const salRound = 10;
 
@@ -16,87 +15,89 @@ app.use(cors({
   origin: 'http://localhost:3000',
   credentials: true 
 }));
-app.use(express.json())
+app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/auth", auth, (req, res) => {
-  console.log(req.user._id);
-  res.send(req.user._id)
-})
+// Middleware to check authentication
+const authenticate = (req, res, next) => {
+  const token = req.cookies.token;
+  if (!token) {
+    return res.status(401).json({ message: "Authentication required" });
+  }
+  try {
+    const decoded = jwt.verify(token, "this is our secret key");
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Invalid token" });
+  }
+};
+
+app.get("/checkLoggedIn", authenticate, (req, res) => {
+  res.json({ isLoggedIn: true, userId: req.user.id });
+});
 
 app.post("/register", async function (req, res) {
   const { email, password } = req.body;
   try {
-      const existingUser = await User.findOne({ email: email });
-      if (existingUser) {
-          return res.status(409).send("User already exists.");
-      }
-      const hashedPassword = await bcrypt.hash(password, salRound);
-      const newUser = await User.create({
-          email: email,
-          password: hashedPassword
-      });
-      const token = jwt.sign(
-          { id: newUser._id, email: email },
-          "this is our secret key",
-          { expiresIn: '1h' }
-      );
-      newUser.token = token;
-      // Cookies section
-      const options = {
-          expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
-          httpOnly: true
-      };
-      console.log(newUser._id);
-      res.status(201).cookie("token", token, options).send(newUser._id);
+    const existingUser = await User.findOne({ email: email });
+    if (existingUser) {
+      return res.status(409).send("User already exists.");
+    }
+    const hashedPassword = await bcrypt.hash(password, salRound);
+    const newUser = await User.create({
+      email: email,
+      password: hashedPassword
+    });
+    const token = jwt.sign(
+      { id: newUser._id, email: email },
+      "this is our secret key",
+      { expiresIn: '1h' }
+    );
+    const options = {
+      expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+      httpOnly: true
+    };
+    res.status(201).cookie("token", token, options).json({ userId: newUser._id });
   } catch (err) {
-      res.status(500).send("Something went wrong.");
+    res.status(500).send("Something went wrong.");
   }
 });
 
-
 app.post('/logout', (req, res) => {
-  res.cookie('token', '', { expires: new Date(0) });
+  res.clearCookie('token');
   res.status(200).send('');
 });
 
 app.post("/login", async function (req, res) {
   const { email, password } = req.body;
   try {
-      const user = await User.findOne({ email: email });
-      if (!user) {
-          return res.status(404).send("User not found.");
-      }
-      const match = await bcrypt.compare(password, user.password);
-      if (match) {
-          const token = jwt.sign(
-              { id: user._id, email },
-              "this is our secret key",
-              { expiresIn: '1h' }
-          );
-          user.token = token;
-          // cookies section
-          const options = {
-              expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
-              httpOnly: true,
-          };
-          console.log(user._id);
-          res.status(200).cookie("token", token, options).send(user._id);
-      } else {
-          res.status(401).send("Invalid credentials.");
-      }
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      return res.status(404).send("User not found.");
+    }
+    const match = await bcrypt.compare(password, user.password);
+    if (match) {
+      const token = jwt.sign(
+        { id: user._id, email },
+        "this is our secret key",
+        { expiresIn: '1h' }
+      );
+      const options = {
+        expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+        httpOnly: true,
+      };
+      res.status(200).cookie("token", token, options).json({ userId: user._id });
+    } else {
+      res.status(401).send("Invalid credentials.");
+    }
   } catch (err) {
-      res.status(500).send("Something went wrong.");
+    res.status(500).send("Something went wrong.");
   }
 });
 
-
-
-
-
-
-app.post("/user/:userId/note", async (req, res) => {
+app.post("/user/:userId/note", authenticate, async (req, res) => {
   const userId = req.params.userId;
   const { title, content } = req.body;
 
@@ -111,8 +112,7 @@ app.post("/user/:userId/note", async (req, res) => {
   }
 });
 
-
-app.get("/user/:userId/note", async (req, res) => {
+app.get("/user/:userId/note", authenticate, async (req, res) => {
   const userId = req.params.userId;
 
   try {
@@ -123,23 +123,21 @@ app.get("/user/:userId/note", async (req, res) => {
   }
 });
 
+app.delete("/user/:userId/note/:noteId", authenticate, async (req, res) => {
+  const { userId, noteId } = req.params;
+  try {
+    const user = await User.findById(userId);
 
-app.delete("/user/:userId/note/:noteId", async (req, res) => {
-    const { userId, noteId } = req.params;
-    try {
-      const user = await User.findById(userId);
+    user.notes.splice(noteId, 1);
+    await user.save();
 
-      user.notes.splice(noteId, 1);
-      await user.save();
-  
-      res.send(user.notes);
-    } catch (error) {
-      res.status(500).send("Error deleting user's note");
-    }
+    res.send(user.notes);
+  } catch (error) {
+    res.status(500).send("Error deleting user's note");
+  }
 });
 
-
-app.patch("/user/:userId/note/:noteId", async (req, res) => {
+app.patch("/user/:userId/note/:noteId", authenticate, async (req, res) => {
   const { userId, noteId } = req.params;
   const { title, content } = req.body;
 
